75f82da6f48d1ca2783e7237e4c67d8a4e54f2cb
clarkjo12 <clarkjo12@gmail.com>
2023-01-31 12:16:07 -0500

    compat2

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -20,13 +20,14 @@
 // const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
 // export { projectStorage, projectFirestore, timestamp, app };
-
+import firebase from "firebase/compat/app";
 /////
-import  firebase from "firebase/app"
+//import  firebase from "firebase/app"
 //import firebase from "firebase/compat/app";
 import "firebase/compat/storage";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
+
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
