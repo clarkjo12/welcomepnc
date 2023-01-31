0f640ab652f1b21d7a9b129c9ef3b09f01d879df
clarkjo12 <clarkjo12@gmail.com>
2023-01-31 15:10:37 -0500

    zoomer

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -31,11 +31,21 @@
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
+////////////////////////////////////////////////////
+///////////////////////////////////////////////////
+import * as firebase from "@firebase/app";
+import { initializeApp } from "@firebase/app";
+import {} from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+
+import "firebase/compat/storage";
+import "firebase/compat/firestore";
+
+// import "firebase/firestore";
+// import "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -50,43 +60,14 @@ const firebaseConfig = {
   measurementId: "G-F4QHPSBEY3"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-////////////////////////////////////////////////////
-///////////////////////////////////////////////////
-// import { initializeApp } from "@firebase/app";
-// import {} from "firebase/auth";
-// import { getFirestore } from "@firebase/firestore";
-// import firebase from "firebase/compat/app";
-// import "firebase/compat/storage";
-// import "firebase/compat/firestore";
+//const analytics = getAnalytics(app);
 
-// // import "firebase/firestore";
-// // import "firebase/auth";
-// // import { initializeApp } from "firebase/app";
-// // import { getFirestore } from 'firebase/firestore/lite';
+const projectStorage = firebase.storage();
+const projectFirestore = getFirestore(app);
+const auth = firebase.auth();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBopSCDwvMnRka6UmnHl-cBggSVs3T-auw",
-//   authDomain: "welcome-home-f1f37.firebaseapp.com",
-//   databaseURL: "https://welcome-home-f1f37.firebaseio.com",
-//   projectId: "welcome-home-f1f37",
-//   storageBucket: "welcome-home-f1f37.appspot.com",
-//   messagingSenderId: "584932731101",
-//   appId: "1:584932731101:web:bf1aee4a7714a46e52d334",
-//   measurementId: "G-F4QHPSBEY3"
-// };
-
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// const projectStorage = firebase.storage();
-// const projectFirestore = getFirestore(app);
-// const auth = firebase.auth();
-// const timestamp = firebase.firestore.FieldValue.serverTimestamp;
-
-// export { projectStorage, projectFirestore, timestamp, app, auth };
+export { projectStorage, projectFirestore, timestamp, app, auth };
