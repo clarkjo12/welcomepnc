030dd753fdd920c2bb6752875016cf9bd3188afc
clarkjo12 <clarkjo12@gmail.com>
2023-01-31 14:31:34 -0500

    peas

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,3 @@
-import * as firebase from "firebase/compat/app";
-import "firebase/compat/storage";
-import "firebase/compat/firestore";
-import "firebase/firestore";
-import "firebase/auth";
 
 
 // const firebaseConfig = {
@@ -36,6 +31,13 @@ import "firebase/auth";
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+import "firebase/compat/firestore";
+// import "firebase/firestore";
+// import "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
