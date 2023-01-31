b2d92057028fc1d9dfbb1fe2c87bd324b13b7748
clarkjo12 <clarkjo12@gmail.com>
2023-01-31 12:40:46 -0500

    maybe4

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -30,8 +30,8 @@ import "firebase/auth";
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -51,7 +51,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
