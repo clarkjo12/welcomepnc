76d375ba1f4c0513a70ecec30d2e957a744a3fc3
clarkjo12 <clarkjo12@gmail.com>
2023-01-31 13:54:41 -0500

    updated firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -30,6 +30,7 @@ import "firebase/auth";
 
 
 
+
 // Import the functions you need from the SDKs you need
 //import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
